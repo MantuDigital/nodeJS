39e4d91b89f50efcd77be8102c03a8654a4b0db1
MantuDigital <assamcybersolutions@gmail.com>
2024-06-13 11:17:42 +0530

    this is simple NodeJS webApp

---
+++ server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const host = 'localhost';
+const port = 8000; 
+
+const requestListener = function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write("<h1> Hello,Web developers");
+  res.end();
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
---
+++ test.js
@@ -0,0 +1,4 @@
+var arr =[1,2,43,2,4,23];
+for(x in arr){
+    console.log(arr[x]);
+}
